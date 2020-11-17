5094895459b8c6c19c4626a3360738dbfde7f412
freejoe <joe.wong2016@outlook.com>
2020-11-17 11:09:57 +0800

    * supplement README with Longer note. * modified configuration.h for define print model

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -108,10 +108,22 @@
 	#ifdef serial_port1
 		#define LGT_MAC	
 		#ifdef LGT_MAC
-			//LK1_Pro
-			// #define LK1_Pro
-         #define LK4_Pro
+			
+        // uncomment or comment LKx_Pro definition to change model
+        // NOTE: shold only define one single model in the meantime
+			  // #define LK1_Pro
+        #define LK4_Pro
         // #define LK5_Pro
+
+        // validation model definition
+        #if defined(LK1_Pro) && !defined(LK4_Pro) && !defined(LK5_Pro)
+        #elif !defined(LK1_Pro) && defined(LK4_Pro) && !defined(LK5_Pro)
+        #elif !defined(LK1_Pro) && !defined(LK4_Pro) && defined(LK5_Pro)
+        #elif !defined(LK1_Pro) && !defined(LK4_Pro) && !defined(LK5_Pro)
+          #error "Not define any one printer model"
+        #else
+          #error "Defined mutiple printer model at the same time"
+        #endif
 
 				#ifdef LK1_Pro
 					#define LK1_Pro_AutoBed
